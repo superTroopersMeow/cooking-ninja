d50efae3f40890fac723078740fe837c5bd408f2
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-15 13:50:29 +0100

    search results page
    imported useLocation .
    created new const function to pull url data with a question query..
    and useFetch for url

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,21 +1,24 @@
-import {Link} from "react-router-dom"
+import { Link } from 'react-router-dom'
 
-//styles
-import "./RecipeList.css"
+// styles
+import './RecipeList.css'
 
+export default function RecipeList({ recipes }) {
 
+  if (recipes.length === 0) {
+    return <div className="error">No recipes to load...</div>
+  }
 
-export default function RecipeList({recipes}) {
   return (
     <div className="recipe-list">
-        {recipes.map(recipe => (
-            <div key={recipe.id} className="card">
-                <h3>{recipe.title}</h3>
-                <p>{recipe.cookingTime} to make.</p>
-                <div>{recipe.method.substring(0, 100)}...</div>
-                < Link to = {`/recipes/${recipe.id}`} > Cook This </Link>
-            </div>
-        ))}
+      {recipes.map(recipe => (
+        <div key={recipe.id} className="card">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+        </div>
+      ))}
     </div>
   )
 }

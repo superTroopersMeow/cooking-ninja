5e6ea753ae66f589200949ac97576642a894f72f
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-15 14:32:26 +0100

    Access context value
    Using useContext hook  to parse in data and access theme that wraps the apllication.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,25 @@
 import {Link} from "react-router-dom"
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+
 
 //styles
 import  "./Navbar.css"
+
 
 //componenets
 import Searchbar from './Searchbar'
 
 export default function Navbar() {
+    const { color } = useContext(ThemeContext)
+    
     return (
-        <div className= "navbar">
+        <div className="navbar" style={{ background: color }}>
         <nav>
         <Link to="/" className="brand" >
         <h1> Cooking Ninja </h1>
         </Link>
-        < Searchbar / >
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
         </nav>
         </div>

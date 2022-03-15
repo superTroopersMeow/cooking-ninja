be38088dfb0a1b96a77801e507cfb4205899e873
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-15 14:42:14 +0100

    Creating a custom context hook
    created a hook in a new useTheme,js to use extra logic to use in navbar component. added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,24 @@
-import {Link} from "react-router-dom"
-import { useContext } from 'react'
-import { ThemeContext } from '../context/ThemeContext'
 
+   
+import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
 
-//styles
-import  "./Navbar.css"
-
-
-//componenets
+// styles
+import './Navbar.css'
 import Searchbar from './Searchbar'
 
 export default function Navbar() {
-    const { color } = useContext(ThemeContext)
-    
-    return (
-        <div className="navbar" style={{ background: color }}>
-        <nav>
-        <Link to="/" className="brand" >
-        <h1> Cooking Ninja </h1>
+  const { color } = useTheme()
+
+  return (
+    <div className="navbar" style={{ background: color }}>
+      <nav>
+        <Link to="/" className="brand">
+          <h1>Cooking Ninja</h1>
         </Link>
         <Searchbar />
         <Link to="/create">Create Recipe</Link>
-        </nav>
-        </div>
-    )
+      </nav>
+    </div>
+  )
 }
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,10 +1,12 @@
 
-   
 import { createContext } from 'react'
 
 export const ThemeContext = createContext()
 
 export function ThemeProvider({ children }) {
+
+    //custom logic
+
 
   return (
     <ThemeContext.Provider value={{ color: 'blue' }}>
